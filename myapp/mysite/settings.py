9957e6fea3923625743cf5537872e553c1bcf50e
Max Verchynskyi <disablaksteam@gmail.com>
2025-05-30 15:06:42 +0300

    removed shell commands and refa

--- myapp/mysite/settings.py
+++ myapp/mysite/settings.py
@@ -38,6 +38,10 @@ ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split("
 
 CSRF_TRUSTED_ORIGINS = os.getenv("CSRF_TRUSTED_ORIGINS", "localhost").split(",")
 
+DJANGO_SUPERUSER_USERNAME = os.getenv("DJANGO_SUPERUSER_USERNAME", "admin")
+DJANGO_SUPERUSER_EMAIL = os.getenv("DJANGO_SUPERUSER_EMAIL", "admin@test.com")
+DJANGO_SUPERUSER_PASSWORD = os.getenv("DJANGO_SUPERUSER_PASSWORD", "adminpassword")
+
 INSTALLED_APPS = [
     "polls.apps.PollsConfig",
     "django.contrib.admin",

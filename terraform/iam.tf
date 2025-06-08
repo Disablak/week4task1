b7953ada007911cd09399a76499a0f7f2ac4bf99
Max Verchynskyi <disablaksteam@gmail.com>
2025-06-08 15:52:18 +0300

    added policy to use password

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -23,7 +23,42 @@ resource "aws_iam_role_policy_attachment" "policy_2" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2RoleforSSM"
 }
 
+resource "aws_iam_role_policy_attachment" "policy_3_to_use_secrets" {
+  role       = aws_iam_role.ssm_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2RoleforSSM"
+}
+
 resource "aws_iam_instance_profile" "ssm_instance_profile" {
   name = "EC2SSMInstanceProfile"
   role = aws_iam_role.ssm_role.name
+}
+
+### Added polciy to use password from SSM
+resource "aws_iam_policy" "smm_access_db_password" {
+  name = "AccessDBPassword"
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "ssm:GetParameter"
+        ],
+        Resource = "arn:aws:ssm:us-east-1:400375466624:parameter/django-app-db-password"
+      },
+      {
+        Effect = "Allow",
+        Action = [
+          "kms:Decrypt"
+        ],
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "attach_ssm_to_django_role" {
+  role       = aws_iam_role.ssm_role.name
+  policy_arn = aws_iam_policy.smm_access_db_password.arn
 }

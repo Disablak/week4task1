94bbd19aa39fdca1bc9da70a933e94f556413913
Max Verchynskyi <disablaksteam@gmail.com>
2025-06-09 11:35:16 +0300

    added s3 for logs and cloudtrail

---
+++ terraform/cloudtrail.tf
@@ -0,0 +1,8 @@
+resource "aws_cloudtrail" "main" {
+  name                          = "my-cloudtrail"
+  s3_bucket_name                = aws_s3_bucket.cloudtrail_bucket.bucket
+  include_global_service_events = true
+  is_multi_region_trail         = true
+  enable_log_file_validation    = true
+  enable_logging                = true
+}
---
+++ terraform/s3.tf
@@ -0,0 +1,43 @@
+resource "aws_s3_bucket" "cloudtrail_bucket" {
+  bucket = "my-cloudtrail-logs-bucket"
+
+  force_destroy = true
+  lifecycle {
+    prevent_destroy = false
+  }
+}
+
+resource "aws_s3_bucket_policy" "cloudtrail_policy" {
+  bucket = aws_s3_bucket.cloudtrail_bucket.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Sid       = "AWSCloudTrailAclCheck20131101",
+        Effect    = "Allow",
+        Principal = {
+          Service = "cloudtrail.amazonaws.com"
+        },
+        Action    = "s3:GetBucketAcl",
+        Resource  = aws_s3_bucket.cloudtrail_bucket.arn
+      },
+      {
+        Sid       = "AWSCloudTrailWrite20131101",
+        Effect    = "Allow",
+        Principal = {
+          Service = "cloudtrail.amazonaws.com"
+        },
+        Action    = "s3:PutObject",
+        Resource  = "${aws_s3_bucket.cloudtrail_bucket.arn}/AWSLogs/${data.aws_caller_identity.current.account_id}/*",
+        Condition = {
+          StringEquals = {
+            "s3:x-amz-acl" = "bucket-owner-full-control"
+          }
+        }
+      }
+    ]
+  })
+}
+
+data "aws_caller_identity" "current" {}

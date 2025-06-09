796ef71e83eccbb3f0e0abfabcf937263e9c78f6
Max Verchynskyi <disablaksteam@gmail.com>
2025-06-09 13:45:12 +0300

    s3 fixes

--- terraform/cloudtrail.tf
+++ terraform/cloudtrail.tf
@@ -1,8 +1,8 @@
 resource "aws_cloudtrail" "main" {
   name                          = "my-cloudtrail"
-  s3_bucket_name                = aws_s3_bucket.cloudtrail_bucket.bucket
+  s3_bucket_name                = module.cloudtrail_s3_bucket.bucket_id
   include_global_service_events = true
-  is_multi_region_trail         = false
+  is_multi_region_trail         = true
   enable_log_file_validation    = true
   enable_logging                = true
 }
--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,43 +1,8 @@
-resource "aws_s3_bucket" "cloudtrail_bucket" {
-  bucket = "my-cloudtrail-logs-bucket"
-  force_destroy = true
-  
-  lifecycle {
-    prevent_destroy = false
-  }
+module "cloudtrail_s3_bucket" {
+  source  = "cloudposse/cloudtrail-s3-bucket/aws"
+  version = "0.27.0"  # Last Version
+
+  namespace = "eg"
+  stage     = "prod"
+  name      = "bucket-for-crloudtrail"
 }
-
-resource "aws_s3_bucket_policy" "cloudtrail_policy" {
-  bucket = aws_s3_bucket.cloudtrail_bucket.id
-
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Sid       = "AWSCloudTrailAclCheck20131101",
-        Effect    = "Allow",
-        Principal = {
-          Service = "cloudtrail.amazonaws.com"
-        },
-        Action    = "s3:GetBucketAcl",
-        Resource  = aws_s3_bucket.cloudtrail_bucket.arn
-      },
-      {
-        Sid       = "AWSCloudTrailWrite20131101",
-        Effect    = "Allow",
-        Principal = {
-          Service = "cloudtrail.amazonaws.com"
-        },
-        Action    = "s3:PutObject",
-        Resource  = "${aws_s3_bucket.cloudtrail_bucket.arn}/AWSLogs/${data.aws_caller_identity.current.account_id}/*",
-        Condition = {
-          StringEquals = {
-            "s3:x-amz-acl" = "bucket-owner-full-control"
-          }
-        }
-      }
-    ]
-  })
-}
-
-data "aws_caller_identity" "current" {}
